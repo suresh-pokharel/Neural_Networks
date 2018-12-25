f43d1dcc924f8674a1487fe03ad39b83a61e89b1
Suresh Pokharel <suresh.wrc@gmail.com>
2018-12-25 21:10:41 +0545

    Create AND_implementation_Hebbian_Learning.py

---
+++ AND_implementation_Hebbian_Learning.py
@@ -0,0 +1,17 @@
+# AND gate implementation using Hebbian learning 
+
+data = [[1, 1, 1, 1],[1, -1, 1, -1],[-1, 1, 1, -1],[-1, -1, 1, -1]] #[]
+
+w1=0
+w2=0
+b=0
+for i in range(4):
+  w1 = w1+data[i][0]*data[i][3]
+  w2 = w2+data[i][1]*data[i][3]
+  b = b+1*data[i][3] # bias is 1 for all inputs
+
+  print("Iteration-"+str(i)+"########")
+  print(data[i])
+  print("w1="+str(w1))
+  print("w2="+str(w2))
+  print("b="+str(b))
